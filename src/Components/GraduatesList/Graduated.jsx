d1d74adb4e96dc0cc9d343013ef1c9234a8bf2a2
Carlos <carlos@MacBook-Pro-de-Carlos.local>
2020-09-12 21:35:55 -0400

    Added list of graduates with API FakeBackendNahual

--- src/Components/GraduatesList/Graduated.jsx
+++ src/Components/GraduatesList/Graduated.jsx
@@ -1,25 +1,18 @@
 import React from "react";
-import { Checkbox, Table, Dropdown } from "semantic-ui-react";
+import { Checkbox, Table } from "semantic-ui-react";
 
 function Graduated(props) {
-  return (
-    <Table.Row>
-      <Table.Cell style={{ textAlign: "center" }}>
-        <Checkbox />
-      </Table.Cell>
-      <Table.Cell>{props.item.name}</Table.Cell>
-      <Table.Cell>{props.item.username}</Table.Cell>
-      <Table.Cell>{props.item.email}</Table.Cell>
-      <Table.Cell>{props.item.address.city}</Table.Cell>
-      <Table.Cell style={{ textAlign: "center" }}>
-        <Dropdown icon="ellipsis vertical">
-          <Dropdown.Menu>
-            <Dropdown.Item>Exportar</Dropdown.Item>
-            <Dropdown.Item>Ver Egresade</Dropdown.Item>
-          </Dropdown.Menu>
-        </Dropdown>
-      </Table.Cell>
-    </Table.Row>
-  );
+	return (
+		<Table.Row>
+			<Table.Cell style={{ textAlign: "center" }}>
+				<Checkbox />
+			</Table.Cell>
+			<Table.Cell>{props.item.name}</Table.Cell>
+			<Table.Cell>{props.item.nodo}</Table.Cell>
+			<Table.Cell>{props.item.moduleCompleted[0].course}</Table.Cell>
+			<Table.Cell>{props.item.englishLevel}</Table.Cell>
+			<Table.Cell style={{ textAlign: "center" }}></Table.Cell>
+		</Table.Row>
+	);
 }
 export default Graduated;
