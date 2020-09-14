ffb764a5cc70dc0250087c3bd597f5db7d79f669
FidelRojas <45080814+FidelRojas@users.noreply.github.com>
2020-09-14 13:48:54 -0400

    Added sections of modal for GraduteDatail

--- src/Components/GraduateDetail/GraduateDetail.jsx
+++ src/Components/GraduateDetail/GraduateDetail.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Modal, Button } from 'semantic-ui-react'
+import { Button, Icon, Modal, Grid, Image } from 'semantic-ui-react'
+import LogoNahual from '../../assets/logo-proyecto-nahual.webp'
 
 const GraduateDetail = ({ graduate }) => {
     const [open, setOpen] = React.useState(false)
@@ -12,7 +13,33 @@ const GraduateDetail = ({ graduate }) => {
             closeIcon
             trigger={<Button icon='eye' />}
         >
-            <h1>Esto es un modal, y el graduado es {graduate.name}</h1>
+            {graduate ?
+                <>
+                    <Modal.Header >
+                        <Image src={LogoNahual} size='small' />
+                    </Modal.Header>
+                    <Modal.Content scrolling>
+                        <Grid divided='vertically'>
+                            
+                        </Grid>
+                    </Modal.Content>
+                    <Modal.Actions>
+                        <Button onClick={() => setOpen(false)} basic >
+                            Atras
+                </Button>
+                        {graduate.Linkedin ?
+                            <Button color='linkedin'>
+                                <Icon name='linkedin' /> LinkedIn
+                    </Button>
+                            :
+                            <Button disabled color='linkedin'>
+                                <Icon name='linkedin' /> LinkedIn
+                    </Button>
+                        }
+                    </Modal.Actions>
+                </>
+                : <h1>Cargando...</h1>
+            }
         </Modal>
     )
 }
