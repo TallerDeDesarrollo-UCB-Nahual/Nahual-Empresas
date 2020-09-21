d017caa690def3156de7660ad17eaa5878b964dd
GamalSerhan <53912162+GamalSerhan@users.noreply.github.com>
2020-09-21 11:18:46 -0400

    Added enviroment variable and changed axios links, also added newFilterRequest

--- src/Services/Services-Graduates/GraduateService.js
+++ src/Services/Services-Graduates/GraduateService.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
+const DATA_SERVICE_API_NAHUAL = process.env.REACT_APP_API_URL;
+
 const GraduateService = {
   GetGraduates(){
-    return axios.get('https://shielded-sands-50510.herokuapp.com/api/graduates/unemployes')
+    return axios.get(`${DATA_SERVICE_API_NAHUAL}/graduates/unemployes`)
   }
 }
 
