60bda0c2fb76db3e675f6739093150e4f505c2b5
Jeymi Paulas <jey.pc.07@gmail.com>
2020-09-21 12:44:33 -0400

    Remove unnecesary imports

--- src/Components/GraduatesList/FilterButton.jsx
+++ src/Components/GraduatesList/FilterButton.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Dropdown, Menu } from "semantic-ui-react";
+import { Dropdown } from "semantic-ui-react";
 import OptionsModuleCompleted from "../FilterGraduates/OptionsModuleCompleted";
 import OptionsEnglishLevel from "../FilterGraduates/OptionsEnglishLevel";
 import OptionsNode from "../FilterGraduates/OptionsNode";
