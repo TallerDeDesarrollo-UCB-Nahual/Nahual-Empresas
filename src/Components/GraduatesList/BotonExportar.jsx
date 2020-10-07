94562cad453abec2571b6bb4c0efe3e84a4fbc6a
melissafg <melissa.flores.g@ucb.edu.bo>
2020-10-07 14:48:12 +0200

    Se configuro las columnas del archivo

--- src/Components/GraduatesList/BotonExportar.jsx
+++ src/Components/GraduatesList/BotonExportar.jsx
@@ -1,14 +1,29 @@
 import React from 'react'
 import ReactExport from 'react-export-excel';
+import moment from 'moment';
+import 'moment/locale/es';
 
 const ArchivoExcel = ReactExport.ExcelFile;
 const HojaExcel = ReactExport.ExcelFile.ExcelSheet;
+const ColumnaExcel = ReactExport.ExcelFile.ExcelColumn;
 
 const BotonDescargar = ({ seleccionados }) => {
+  moment.locale('es');
+  const fecha_descarga = moment(Date.now()).format('LL');
   return (
     <div>
-      <ArchivoExcel filename = {`Graduades Exportados - ${Date.now()}`}>
+      <ArchivoExcel filename={`Graduades Exportados - ${fecha_descarga}`}>
         <HojaExcel data={seleccionados} name="Graduades">
+          <ColumnaExcel label="Nombre" value="fullName" />
+          <ColumnaExcel label="Correo" value="mail" />
+          <ColumnaExcel label="Teléfono" value="cellphone" />
+          <ColumnaExcel label="Fecha de nacimiento" value="birthDate" />
+          <ColumnaExcel label="Nivel de inglés" value="englishLevel" />
+          <ColumnaExcel label="Nodo:" value="node" />
+          <ColumnaExcel label="Sede" value="nodeName" />
+          <ColumnaExcel label="Linkedin" value="linkedin" />
+          <ColumnaExcel label="Curso Realizádo" value="finishedCourses" />
+          <ColumnaExcel label="Año" value="graduationYear" />
         </HojaExcel>
       </ArchivoExcel>
     </div>
