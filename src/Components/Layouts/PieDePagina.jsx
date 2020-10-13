45d6510fcf1db5ed369ae1db7345415c9c24ad9d
FidelRojas <45080814+FidelRojas@users.noreply.github.com>
2020-10-12 23:35:22 -0400

    Se corrigió el pie de pagina y mayusculas en la tabla

--- src/Components/Layouts/PieDePagina.jsx
+++ src/Components/Layouts/PieDePagina.jsx
@@ -1,23 +1,18 @@
 import React from "react";
 import NahualLogo from "../../assets/logo-proyecto-nahual.webp";
 
-import {
-  Container,
-  Image,
-  List,
-  Segment
-} from "semantic-ui-react";
+import { Container, Image, List, Segment } from "semantic-ui-react";
 
 const PieDePagina = (props) => {
   return (
     <Segment
       inverted
       vertical
-      style={{ margin: "5em 0em 0em", padding: "5em 0em" }}
+      style={{ margin: "5em 0em 0em", padding: "3em 0em" }}
     >
       <Container textAlign="center">
         <Image centered size="small" src={NahualLogo} />
-        <List horizontal  divided link size="small">
+        <List horizontal divided link size="small">
           <List.Item as="a" href="https://www.nahual.com.ar/">
             Nahual Argentina
           </List.Item>
