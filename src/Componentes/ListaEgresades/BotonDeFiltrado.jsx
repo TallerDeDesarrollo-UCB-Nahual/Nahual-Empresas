d68874ad2cdabccd0c3d4ca02457b4d0ee981ae1
Carlos Manuel Luna Condori <kuko.cmlc@gmail.com>
2020-10-13 00:26:35 -0400

    Aplicando formatos al codigo

--- src/Componentes/ListaEgresades/BotonDeFiltrado.jsx
+++ src/Componentes/ListaEgresades/BotonDeFiltrado.jsx
@@ -4,23 +4,35 @@ import OpcionesDeModulosCompletados from "../FiltradoDeEgresades/OpcionesDeModul
 import OpcionesDeNivelDeIngles from "../FiltradoDeEgresades/OpcionesDeNivelDeIngles";
 import OpcionesDeNodo from "../FiltradoDeEgresades/OpcionesDeNodo";
 
-function BotonDeFiltrado(props){
+function BotonDeFiltrado(props) {
   return (
-    <Dropdown 
-      text= 'Filtrar'
-      icon='filter'
+    <Dropdown
+      text="Filtrar"
+      icon="filter"
       floating
       labeled
       button
-      className='icon'
+      className="icon"
     >
-      <Dropdown.Menu style={{width:"165px"}}>
-        <OpcionesDeNodo manejarEvento={props.manejarEvento} valor={props.valor} quitarUnFiltro={props.quitarUnFiltro} />
-        <OpcionesDeModulosCompletados manejarEvento={props.manejarEvento} valor={props.valor} quitarUnFiltro={props.quitarUnFiltro} />
-        <OpcionesDeNivelDeIngles manejarEvento={props.manejarEvento} valor={props.valor} quitarUnFiltro={props.quitarUnFiltro} />
+      <Dropdown.Menu style={{ width: "165px" }}>
+        <OpcionesDeNodo
+          manejarEvento={props.manejarEvento}
+          valor={props.valor}
+          quitarUnFiltro={props.quitarUnFiltro}
+        />
+        <OpcionesDeModulosCompletados
+          manejarEvento={props.manejarEvento}
+          valor={props.valor}
+          quitarUnFiltro={props.quitarUnFiltro}
+        />
+        <OpcionesDeNivelDeIngles
+          manejarEvento={props.manejarEvento}
+          valor={props.valor}
+          quitarUnFiltro={props.quitarUnFiltro}
+        />
       </Dropdown.Menu>
     </Dropdown>
-    );
-  }
+  );
+}
 
 export default BotonDeFiltrado;
